7ce4a34484495e6f01b371b70f897e8cfc804b0e
ganqing.wang <wangganqing92@163.com>
2023-12-03 19:49:12 +0800

    fix: 日常更新

---
+++ node/6-module/npm/index.js
@@ -0,0 +1,7 @@
+const arr = [1, 2, 1, 6, 4, 2, 1, 3, 4, 5, 6, 3, 2]
+
+const uniq = require('uniq')
+// 等价于 uniq = require('./node_modules/uniq')
+// 因为./node_modules/uniq中package.json的main属性指向uniq.js, 所以也等价于uniq = require('./node_modules/uniq/uniq')
+
+console.log(uniq(arr))
---
+++ node/6-module/npm/require.js
@@ -0,0 +1,3 @@
+const uniq = require('./node_modules/uniq')
+const arr = [1, 2, 1, 6, 4, 2, 1, 3, 4, 5, 6, 3, 2]
+console.log(uniq(arr))
