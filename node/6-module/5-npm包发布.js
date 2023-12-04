c14f7af40f0f6ea155b255e4876cae09e72b63fb
ganqing.wang <wangganqing92@163.com>
2023-12-04 09:38:46 +0800

    fix: 日常更新

---
+++ node/6-module/5-npm包发布.js
@@ -0,0 +1,5 @@
+// npm init
+// npm login
+// npm publish
+
+// npm 账号: snow_leopard_v i+love+pjacl1314
---
+++ node/6-module/npm/publish.js
@@ -0,0 +1,3 @@
+const { snowLeopardPublish, add } = require('snow_leopard_publish')
+
+console.log(3, add(1, 2))
