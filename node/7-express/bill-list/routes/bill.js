6b87fefdf64f73bf045092f7a5ab7d7d0740513e
ganqing.wang <wangganqing92@163.com>
2023-12-05 18:11:57 +0800

    fix: 日常更新

---
+++ node/7-express/bill-list/routes/bill.js
@@ -0,0 +1,42 @@
+var express = require('express')
+var router = express.Router()
+const path = require('path')
+const low = require('lowdb')
+const FileSync = require('lowdb/adapters/FileSync')
+const shortId = require('shortid')
+
+const adapter = new FileSync(path.resolve(__dirname, '../db/db.json'))
+const db = low(adapter)
+
+/* GET users listing. */
+router.get('/list', function (req, res, next) {
+  const bills = db.get('bills').value()
+  console.log(14, bills)
+  res.render('list', { bills })
+})
+
+router.get('/add', function (req, res, next) {
+  res.render('add')
+})
+
+router.post('/add', function (req, res, next) {
+  const bill = {
+    ...req.body,
+    id: shortId.generate(),
+  }
+
+  db.get('bills').unshift(bill).write()
+  res.render('status', { message: '添加' })
+})
+
+router.get('/delete/:id', (request, response) => {
+  const id = request.params.id
+
+  db.get('bills').remove({ id }).write()
+
+  response.render('status', { message: '删除' })
+})
+
+router.use(express.static(path.resolve(__dirname, '../public')))
+
+module.exports = router
