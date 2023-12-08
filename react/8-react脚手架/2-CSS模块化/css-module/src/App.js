1c9adc388e1badb8de9b81df5bb404f1e8e8b90c
ganqing.wang <wangganqing92@163.com>
2023-12-08 22:16:07 +0800

    fix: 日常更新

--- react/8-react脚手架/2-CSS模块化/css-module/src/App.js
+++ react/8-react脚手架/2-CSS模块化/css-module/src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react'
 // 注意: 这里的{}不是解构赋值, 是按需导入
 import Hello from './components/Hello'
+import Welcome from './components/Welcome'
 
 export default class App extends Component {
   render() {
     return (
       <div>
         <Hello />
+        <Welcome />
       </div>
     )
   }
