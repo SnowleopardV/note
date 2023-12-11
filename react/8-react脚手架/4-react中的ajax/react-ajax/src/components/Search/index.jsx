3df7ede93b2ddd1c03ec0056613bc64a1e74d589
ganqing.wang <wangganqing92@163.com>
2023-12-11 09:50:42 +0800

    fix: 日常更新

--- react/8-react脚手架/4-react中的ajax/react-ajax/src/components/Search/index.jsx
+++ react/8-react脚手架/4-react中的ajax/react-ajax/src/components/Search/index.jsx
@@ -15,14 +15,23 @@ export default class Search extends Component {
     //   PubSub.publish('listStateChange', { isFirst: false, isLoading: false, error: error.message })
     // })
 
-    // fetch写法
+    // fetch写法, 原生API, 关注分离的思想
     fetch(`http://api.github.com/search/users?q=${keyWord}`).then(response => {
       console.log(20, response)
-    }, error => {
-      // 
-      console.log(22, error.toString())
+      return response.json() // 这里需要返回response.json()
+    }
+      // , error => {
+      //   console.log(22, error.toString())
+      // }
+    ).then(res => {
+      // 服务器连接正常
+      console.log(26, res)
+      PubSub.publish('listStateChange', { users: res?.items, isLoading: false })
+    }).catch(error => {
+      // 这里的catch表示服务器连接异常
+      console.log(27, error.toString())
+      PubSub.publish('listStateChange', { isLoading: false, error: error.toString() })
     })
-
   }
 
   render () {
