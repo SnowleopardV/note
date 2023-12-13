dd8b2241386fea3ae43273673783c3f16ecb157a
ganqing.wang <wangganqing92@163.com>
2023-12-13 15:23:55 +0800

    fix: 日常更新

--- react/11-react状态管理/1-redux/src/redux/store.js
+++ react/11-react状态管理/1-redux/src/redux/store.js
@@ -1,5 +1,7 @@
 import { legacy_createStore as createStore } from 'redux'
 // createStore 已经被弃用, 改用 legacy_createStore
 import countReduce from './count_reduce'
+// 项目中引入redux-devtools-extensionx
+import { composeWithDevTools } from 'redux-devtools-extension'
 
-export default createStore(countReduce)
+export default createStore(countReduce, composeWithDevTools())
